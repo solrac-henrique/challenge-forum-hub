5bd510aedbdad7a1783ec930e6f4633dc949558e
solrac-henrique <ch08082003@gmail.com>
2024-12-20 16:47:11 -0300

    Create UsuarioController.java

---
+++ UsuarioController.java
@@ -0,0 +1,48 @@
+package api.forumhub.controller;
+
+import api.forumhub.domain.usuario.DadosLogin;
+import api.forumhub.domain.usuario.Usuario;
+import api.forumhub.infra.security.TokenDadosTokenJWT;
+import api.forumhub.infra.security.TokenService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class UsuarioController {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity login(@RequestBody @Valid DadosLogin dados){
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new TokenDadosTokenJWT(tokenJWT));
+    }
+}
+
+
+
+
+
+
+
+
+
+
