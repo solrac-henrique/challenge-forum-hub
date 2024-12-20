05b82dd45a35c96ffcdd1b4301544144a4ba43da
solrac-henrique <ch08082003@gmail.com>
2024-12-20 16:46:05 -0300

    Create UsuarioRepository.java

---
+++ UsuarioRepository.java
@@ -0,0 +1,10 @@
+package api.forumhub.repository;
+
+import api.forumhub.domain.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByLogin(String login);
+}
