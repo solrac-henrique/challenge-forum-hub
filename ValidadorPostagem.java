94721ca36d35549c57234c11ce2f72818f1d22de
solrac-henrique <ch08082003@gmail.com>
2024-12-20 16:50:20 -0300

    Create ValidadorPostagem.java

---
+++ ValidadorPostagem.java
@@ -0,0 +1,8 @@
+package api.forumhub.domain.postagem.validation;
+
+import api.forumhub.domain.postagem.DadosPostagem;
+
+public interface ValidadorPostagem {
+
+    void validador(DadosPostagem dados);
+}
